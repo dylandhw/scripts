bc4026e8b736f9aa5c2e0fea412256bb4b7046c4
dylandhw <dylanwilkins2308@gmail.com>
2025-09-12 05:37:36 -0400

    something fun lol

--- randstr.py
+++ randstr.py
@@ -1,11 +1,15 @@
 import random 
 import string 
 import time 
+from termcolor import colored, cprint 
 
-length = 32 
-for i in range(1):
+length = 75
+for i in range(1000000000):
     chars = string.ascii_letters + string.digits 
     randstr = ''.join(random.choices(chars, k=length))    
     for i in range(len(randstr)):
-        time.sleep(0.1)
-        print(randstr[i])
+        time.sleep(0.01)
+        x = random.randint(0, 255)
+        y = random.randint(0, 255)
+        z = random.randint(0, 255)
+        cprint(randstr, (x, y, z))
