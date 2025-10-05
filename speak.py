e79775dfed725bb61ed9e668610c179b871a8d11
dylandhw <dylanwilkins2308@gmail.com>
2025-10-05 02:06:27 -0400

    refactored

--- speak.py
+++ speak.py
@@ -1,5 +1,7 @@
 import requests 
-import pyttsx3
+import pyttsx3 
+from gtts import gTTS
+import os
 
 def check_internet(url="https://www.google.com", timeout=5):
     try:
@@ -10,6 +12,22 @@ def check_internet(url="https://www.google.com", timeout=5):
     except requests.Timeout:
         return False 
 
-engine = pyttsx3.init()
-engine.say("hello, world. python has given me life!")
-engine.runAndWait()
+def googleTTS():
+    text = "hello world. google text-to-speech has given me life!"
+    tts = gTTS(text)
+    tts.save("hello.mp3")
+    os.system("mpg123 hello.mp3")
+
+def localTTS():
+    engine = pyttsx3.init()
+    engine.say("hello, world. python has given me life!")
+    engine.runAndWait()
+
+def main():
+    if check_internet():
+        googleTTS()
+    else:
+        localTTS()
+
+if __name__ == "__main__":
+    main()
