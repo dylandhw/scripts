a83e71fd69ce285fa082057230dab86b510b5490
dylandhw <dylanwilkins2308@gmail.com>
2025-10-05 01:48:00 -0400

    functionality for lack of internet, sounds very robotic

--- speak.py
+++ speak.py
@@ -1,4 +1,5 @@
 import requests 
+import pyttsx3
 
 def check_internet(url="https://www.google.com", timeout=5):
     try:
@@ -8,3 +9,7 @@ def check_internet(url="https://www.google.com", timeout=5):
         return False 
     except requests.Timeout:
         return False 
+
+engine = pyttsx3.init()
+engine.say("hello, world. python has given me life!")
+engine.runAndWait()
