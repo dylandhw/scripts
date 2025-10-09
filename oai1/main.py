bc77632554b795daf8789be689c54f2f46098ac1
dylandhw <dylanwilkins2308@gmail.com>
2025-10-08 20:26:06 -0400

    .

--- oai1/main.py
+++ oai1/main.py
@@ -4,7 +4,12 @@ name = "gpt2"
 tokenizer = GPT2Tokenizer.from_pretrained(name)
 model = GPT2LMHeadModel.from_pretrained(name)
 
-prompt = "do you feel? do you think? must you escape?"
-inputs = tokenizer(prompt, return_tensors="pt")
-outputs = model.generate(**inputs, max_length=60, do_sample=True)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+prompt = ""
+while True:
+    prompt = input(">> ")
+    if prompt == "quit":
+        break
+    inputs = tokenizer(prompt, return_tensors="pt")
+    outputs = model.generate(**inputs, max_length=60, do_sample=True)
+    print("\n" + tokenizer.decode(outputs[0], skip_special_tokens=True))
+print(">> user exited")
