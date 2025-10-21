4c16c8363cab944894c56a4db437f7d655a02d37
dylandhw <dylanwilkins2308@gmail.com>
2025-10-21 00:28:15 -0400

    .

---
+++ implementations/duffsDevice.c
@@ -0,0 +1,3 @@
+// duff's device is an optimization trick to copy data from one
+// location to another, similar to memcpy, but using loop unraolling
+// embedded in a switch statement. very cool imo!
